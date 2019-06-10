6c42b652df6b5e6dd9c090aa09eb32dc4fe52af1
leb <leb5313@gmail.com>
2019-06-10 12:28:43 -0400

    reinstall all packages for scrape app

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var mongojs = require("mongojs");
 var cherrio = require("cheerio");
 var mongoose = require("mongoose");
 var express = require("express-handlebars");
-
+var axios = require("axios");
 var app = express();
 
 var PORT = process.env.PORT || 3000;
@@ -11,7 +11,26 @@ var PORT = process.env.PORT || 3000;
 var MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-// port listening to
+axios.get("https://www.nytimes.com").then(function(response) {
+  var $ = cheerio.load(response.data);
+  var results = [];
+  $("article").each(function(i, element) {
+    var title = $(element)
+      .children()
+      .text();
+    var link = $(element)
+      .find("a")
+      .attr("href");
+    results.push({
+      title: title,
+      link: link
+    });
+  });
+
+  console.log(results);
+});
+
+port listening to
 app.listen(PORT, function() {
   console.log("Listening on port: " + PORT);
 });
