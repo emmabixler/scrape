4e9e0ee9eae5be7f719edeb392de43cfac864851
leb <leb5313@gmail.com>
2019-06-10 16:18:03 -0400

    run a test for port

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 var axios = require("axios");
+var cherrio = require("cherrio");
 
-axios.get("https://www.nydailynews.com/ ").then(response) => {
-    console.log(response.data)
-};
+console.log("test");
+
+axios.get("https://www.nydailynews.com/ ").then(function(response) {
+  var $ = cherrio.load(response.data);
+});
+
+//empty array for to sava what we collected
+var results = [];
+$("p.title").each(function(i, element) {
+  var title = $(element).text();
+  var link = $(eleement)
+    .childern()
+    .attr("href");
+  results.push({
+    title: title,
+    link: link
+  });
+});
+console.log(results);
--- server.js
+++ server.js
@@ -4,33 +4,65 @@ var cherrio = require("cheerio");
 var mongoose = require("mongoose");
 var express = require("express-handlebars");
 var axios = require("axios");
-var app = express();
+var app = require("express")();
 
-var PORT = process.env.PORT || 3000;
+var databaseURL = "scraper";
+var collections = ["scrapedData"];
 
-var MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-axios.get("https://www.nytimes.com").then(function(response) {
-  var $ = cheerio.load(response.data);
-  var results = [];
-  $("article").each(function(i, element) {
-    var title = $(element)
-      .children()
-      .text();
-    var link = $(element)
-      .find("a")
-      .attr("href");
-    results.push({
-      title: title,
-      link: link
-    });
-  });
-
-  console.log(results);
+var db = mongojs(databaseURL, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
 });
 
-port listening to
-app.listen(PORT, function() {
-  console.log("Listening on port: " + PORT);
+app.get("/", function(req, res) {
+  res.send("Hello world");
+});
+
+//next to put the server in a route function,empty brackets mean find anything and match scarped to id
+app.get("/all,", function(req, res) {
+  db.scrapedData.find({}, function(err, found) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(found);
+    }
+  });
+});
+//route 2- your choice of server
+
+app.get("/scrape", function(req, res) {
+  request("https://www.nytimes.com/", function(error, response, html) {
+    var $ = cheerio.load(html);
+    $(".title").each(function(i, element) {
+      var title = $(this)
+        .childern("a")
+        .text();
+    });
+  });
+});
+// var PORT = process.env.PORT || 3000;
+
+// var MONGODB_URI =
+//   process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// axios.get("https://www.nytimes.com").then(function(response) {
+//   var $ = cheerio.load(response.data);
+//   var results = [];
+//   $("article").each(function(i, element) {
+//     var title = $(element)
+//       .children()
+//       .text();
+//     var link = $(element)
+//       .find("a")
+//       .attr("href");
+//     results.push({
+//       title: title,
+//       link: link
+//     });
+//   });
+
+//   console.log(results);
+// });
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
 });
